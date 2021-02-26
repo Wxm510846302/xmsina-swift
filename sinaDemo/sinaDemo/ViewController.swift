34b374f2ed0c7a383ea30c15a499ed54918c0857
wxm <510846302@qq.com>
2021-02-26 18:18:02 +0800

    add sinademo

---
+++ sinaDemo/sinaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sinaDemo
+//
+//  Created by admin on 2021/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
